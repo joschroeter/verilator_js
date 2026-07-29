ace642a79aeef4926bd1f43ec8bfdf0d9d944c07
Jonathan Schröter <jonathan.schroeter@hm.edu>
2026-07-29 13:49:49 +0200

    test: cover hooking a signal wider than 64 bits

---
+++ test_regress/t/t_ins_dpi_hook_wide.v
@@ -0,0 +1,35 @@
+// DESCRIPTION: Verilator: Verilog Test module
+//
+// This file ONLY is placed under the Creative Commons Public Domain, for
+// any use, without warranty, 2025 by Wilson Snyder.
+// SPDX-FileCopyrightText: 2025 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+
+module top_module;
+  logic         clk = 0;
+  logic [127:0] a = 128'h2;
+  logic [127:0] q;
+
+  sub s0 (.a(a), .q(q));
+
+  int cyc = 0;
+  always #5 clk = ~clk;
+
+  always @(posedge clk) begin
+    cyc <= cyc + 1;
+    $display("$[%0d| q: %h]", cyc, q);
+    if (cyc > 3) begin
+      $display("*-* All Finished *-*");
+      $finish;
+    end
+  end
+endmodule
+
+module sub (
+  input  logic [127:0] a,
+  output logic [127:0] q
+);
+  logic [127:0] v;
+  always_comb v = a;
+  assign q = v;
+endmodule

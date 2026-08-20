28e442a0966c460485f7e23f6aabf6f81a1f8188
Jonathan Schröter <jonathan.schroeter@hm.edu>
2026-08-20 15:16:19 +0200

    test: DPI-hook on a multiply-instantiated interface

--- test_regress/t/t_ins_dpi_hook_iface_multi.v
+++ test_regress/t/t_ins_dpi_hook_iface_multi.v
@@ -9,21 +9,30 @@ interface simple_if;
   logic [7:0] data;
 endinterface
 
-module producer(simple_if b);
-  always_comb b.data = 8'hAA;
+module producer(simple_if b, input logic [7:0] val);
+  always_comb b.data = val;
 endmodule
 
 module top_module;
   logic clk = 0;
   int cyc = 0;
-  simple_if b();
-  simple_if b2();
-  producer p(.b(b));
-  producer p2(.b(b2));
+
+  simple_if s1();
+  simple_if s2();
+  simple_if arr[2]();
+
+  producer p1(.b(s1), .val(8'h10));
+  producer p2(.b(s2), .val(8'h20));
+  producer pa0(.b(arr[0]), .val(8'h30));
+  producer pa1(.b(arr[1]), .val(8'h40));
+
   always #5 clk = ~clk;
+
   always @(posedge clk) begin
     cyc <= cyc + 1;
-    if (cyc > 3) begin
+    $display("$[%0d| s1: %02x s2: %02x a0: %02x a1: %02x]", cyc, s1.data, s2.data, arr[0].data,
+             arr[1].data);
+    if (cyc > 5) begin
       $display("*-* All Finished *-*");
       $finish;
     end

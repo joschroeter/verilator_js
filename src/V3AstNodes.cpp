verilator/verilator

feat: Extendend .json content

src/V3AstNodes.cpp
@@ -2790,6 +2790,8 @@ void AstVar::dumpJson(std::ostream& str) const {
     dumpJsonBoolFunc(str, attrSFormat);
     dumpJsonBoolFunc(str, ignorePostWrite);
     dumpJsonBoolFunc(str, ignoreSchedWrite);
+    dumpJsonBoolFunc(str, isHookInserted);
+    dumpJsonNum(str, "width", width());
     dumpJsonGen(str);
 }
 void AstScope::dump(std::ostream& str) const {

joschroeter/verilator_js

src/V3AstNodes.cpp
@@ -3402,6 +3402,8 @@ void AstVar::dumpJson(std::ostream& str) const {
     dumpJsonBoolFuncIf(str, hasUserInit);
     dumpJsonBoolFuncIf(str, ignorePostWrite);
     dumpJsonBoolFuncIf(str, ignoreSchedWrite);
+    dumpJsonBoolFuncIf(str, isHookInserted);
+    dumpJsonNum(str, "width", width());
     dumpJsonGen(str);
 }
 void AstScope::dump(std::ostream& str) const {